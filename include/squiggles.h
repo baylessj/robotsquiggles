b6f5dc1740290cad531d22743c6062bf53b8648a
Jonathan Bayless <baylessj@purdue.edu>
2020-10-14 19:36:23 -0400

    very basic starter c project

---
+++ include/squiggles.h
@@ -0,0 +1,11 @@
+/**
+ * Copyright 2020 Jonathan Bayless
+ * 
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+#ifndef _ROBOT_SQUIGGLES_H_
+#define _ROBOT_SQUIGGLES_H_
+
+#endif
