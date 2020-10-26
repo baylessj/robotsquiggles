f31d41bce1fe016b7f5bfb7c84bf8b5516a4a266
Jonathan Bayless <baylessjonathan@gmail.com>
2020-10-26 11:05:50 -0400

    add valgrind action + minor code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
-#include "squiggles.hpp"
 #include <iostream>
 
+#include "squiggles.hpp"
+
+using namespace squiggles;
+
 int main() {
+  auto constraints = Constraints(2.0, 2.0, 1.0);
+  auto model = std::make_shared<TankModel>(0.4, constraints);
+  auto spline = Spline(ControlVector(Pose(0, 0, 0), 0.0, 0.0),
+                       ControlVector(Pose(0, 2, 0), 0.0, 0.0),
+                       constraints,
+                       model,
+                       0.1);
+  auto path = spline.plan();
+  auto planned_path = spline.parameterize(path);
   return 0;
 }
